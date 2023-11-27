a28518bdf0b72007323b703f9ece1d84ba75d857
RoyalFoxy <sepp.toni.mo@gmail.com>
2023-11-27 14:04:16 +0100

    I hope this works... Im running out of ideas

--- scripts/get-openai-token.js
+++ scripts/get-openai-token.js
@@ -1,24 +1,34 @@
 import { writeFileSync, existsSync } from "fs";
+import open from "open";
+import readline from "readline";
 
-if (existsSync("src/env.js")) process.exit(0);
+// if (existsSync("src/env.js")) process.exit(0)
 
-const [, , urlString] = process.argv;
+const [, , ipString] = process.argv;
 
-const isProperUrl = /https?:\/\/.*/.test(urlString);
+const isActualIp = /[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/.test(
+  ipString
+);
 
-if (!isProperUrl) {
+if (!isActualIp) {
   console.log(`Du hast dich wahrscheinlich verschrieben.`);
   process.exit(1);
 }
 
-const response = fetch(`${urlString}/api-key`).catch(() => {
-  console.log(
-    "Etwas ist schief gelaufen. Frag jemand von der Liip nach Hilfe."
-  );
-  process.exit(1);
+await open(`http://${ipString}:3002/api-key`);
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
 
-const { token, organization } = await (await response).json();
+const { token, organization } = JSON.parse(
+  await new Promise((resolve) =>
+    rl.question("Hier einfügen: ", (contents) => resolve(contents))
+  )
+);
+
+rl.close();
 
 const envFile = `export const OPENAI_API_KEY = "${token}"; export const OPENAI_ORGANIZATION = "${organization}";`;
 
